05ab808114e35603bf98e52e6dbb1e05c20b6787
Abdul Basit Butt <93610658+basit21740@users.noreply.github.com>
2021-12-26 16:19:00 -0500

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Abdul Basit
+# Created on: Dec 2021
+# this is constant files for Pybadge
+
+
+#PyBadge screen size is 160x128 and sprite.
+SCREEN_X= 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
